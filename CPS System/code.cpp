8c66f6f6fd99438f4df7d5106e1496637c13cd9b
sujalutekar <sujalisosm@gmail.com>
2024-11-12 08:12:00 +0530

    commit

---
+++ CPS System/code.cpp
@@ -0,0 +1,30 @@
+
+int ledPin = 12;  // connect led to this pin on arduino
+
+void setup() 
+{
+  pinMode(ledPin, OUTPUT);   // Set the LED pin as an output
+  Serial.begin(9600);        // Start serial communication at 9600 baud rate
+  //Serial.println("CPS System");  uncomment only for using in simulator
+}
+
+void loop() 
+{
+  if (Serial.available() > 0) 
+  {      
+    // Check if data is available on serial port
+    char command = Serial.read();    // Read the command sent over serial
+    if (command == '1') 
+    {
+      // If command is '1', turn on LED
+      digitalWrite(ledPin, HIGH);
+      Serial.println("LED ON");
+    } 
+    else if (command == '0') 
+    {     
+      // If command is '0', turn off LED
+      digitalWrite(ledPin, LOW);
+      Serial.println("LED OFF");
+    }
+  }
+}
